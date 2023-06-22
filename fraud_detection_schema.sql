5a4a23b7481b85b5fc3be4f732b57684ff84cc5c
treysl <trey.lumley@gmail.com>
2023-06-22 14:26:04 -0500

    cleaned it up

--- fraud_detection_schema.sql
+++ fraud_detection_schema.sql
@@ -1,56 +1,62 @@
 
--- Viewing the columns for the transaction_data table:
+/*
+ * This script provides insights into transaction data for the finance department of a Reputable Company.
+ * The queries are designed to detect fraudulent transactions based on specific suspicious patterns.
+ */
+
+/*
+ * Query 1: View the columns for the transaction_data table
+ */
 SELECT *
 FROM transaction_data
 LIMIT 10;
 
--- The finance department noted that some of the fraudulent transactions were recorded as coming from Smokey Bear’s zip code (20252).
--- You agree this is suspicious, it’s unlikely that the fire prevention mascot is using Reputable Company’s services.
--- Find the full_names and emails of the transactions listing 20252 as the zip code.
--- Find the full_names and emails
--- of the transactions listing 20252 as the zip code.
+/*
+ * Query 2: Find the full names and emails of transactions from zip code 20252
+ * Some fraudulent transactions were noted to originate from this zip code, which is associated with Smokey Bear.
+ */
 SELECT full_name, email, zip
 FROM transaction_data
 WHERE zip = "20252";
 
--- Finance has also noticed a number of pseudonyms associated with fraudulent transactions.
--- The fraudsters thought it would be funny to use ‘Art Vandelay’ for their full name or add a ‘der’ for their middle name.
--- Use a query to find the names and emails associated with these transactions.
--- 3
--- Use a query to find the names 
--- and emails associated with these transactions.
+/*
+ * Query 3: Find the full names and emails associated with pseudonym transactions
+ * The pseudonyms are either 'Art Vandelay' or contain 'der' as the middle name.
+ */
 SELECT full_name, email
 FROM transaction_data
 WHERE full_name = 'Art Vandelay'
    OR full_name LIKE '% der %';
 
--- There are some irregularities in the IP addresses where transactions are originating from.
--- For example, any IP address beginning with ‘10.’ is reserved for internal use.
--- We shouldn’t see IP addresses like this accessing Reputable Company’s service.
--- Find the ip_addresses and emails listed with these transactions.
+/*
+ * Query 4: Find the IP addresses and emails of transactions originating from internal-use IP addresses
+ * Any IP address beginning with '10.' is reserved for internal use.
+ */
 SELECT ip_address, email
 FROM transaction_data
 WHERE ip_address LIKE '10.%';
 
-
--- Users are making fraudulent transactions using a temporary email address service. 
--- These services provide a short-lived email that can be verified and then self-destructs.
--- Find the emails in transaction_data with ‘temp_email.com’ as a domain.
+/*
+ * Query 5: Find the emails using temporary email address service from the transaction data
+ * Fraudulent transactions are being made using these short-lived email addresses.
+ */
 SELECT email
 FROM transaction_data
 WHERE email LIKE '%temp_email.com';
 
--- The finance department is looking for a specific transaction. They know that the transaction occurred from an ip address starting with ‘120.’ and their full name starts with ‘John’.
--- Can you find the transaction?
+/*
+ * Query 6: Find a specific transaction for the finance department
+ * The transaction was made from an IP address starting with '120.' and the full name starts with 'John'.
+ */
 SELECT *
 FROM transaction_data
 WHERE full_name LIKE 'John%'
   AND ip_address LIKE '120.%';
 
--- Additional query practice
--- Return only those customers residing in GA. Use the list of ZIP CODE prefixes
--- (https://en.wikipedia.org/wiki/List_of_ZIP_Code_prefixes)
--- to determine the best query for zip codes belonging to Georgia(GA).
+/*
+ * Query 7: Find customers residing in Georgia (GA) based on ZIP code prefixes
+ * The ZIP code prefixes for Georgia are '30', '31', and '39'.
+ */
 SELECT *
 FROM transaction_data
 WHERE zip LIKE '30%'
