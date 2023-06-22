bd524139f41848d97918aef1c4cac50d68ecb010
treysl <trey.lumley@gmail.com>
2023-06-22 14:24:30 -0500

    Cleaned it up

--- customer_segmentation_schema.sql
+++ customer_segmentation_schema.sql
@@ -1,68 +1,63 @@
--- RPA Customer Segmentation
--- The marketing department of Reputable Product Agency (RPA) is looking to segment the company users by a number of different criteria.
+/*
+ * This script generates user lists for the marketing department of Reputable Product Agency (RPA).
+ * The segmentation is based on different criteria, which are determined by SQL queries.
+ * 
+ * A segment refers to a subset of users that meet different conditions. These are used to send 
+ * marketing campaigns to users who meet specific criteria or to measure the performance of specific 
+ * marketing campaigns.
+ * 
+ * The users dataset is already imported into the workspace.
+ */
 
--- In this context, a segment is a subset of users that meet different conditions. Segments are used to send marketing campaigns to users who meet specific criteria or to measure the performance of specific marketing campaigns.
-
--- Use SQL queries to generate user lists for the marketing department. The users dataset is imported into the workspace.
-
--- The schema of the table is available here.
-
--- If you get stuck during this project or would like to see an experienced developer work through it, click “Get Unstuck“ to see a project walkthrough video.
-
---1
--- What are the column names?
+/*
+ * Query 1: Get the column names in the users table
+ */
 SELECT *
 FROM users
 LIMIT 20;
 
--- 2
--- Find the email addresses and birthdays of users whose 
--- birthday is between 1980-01-01 and 1989-12-31.
-
+/*
+ * Query 2: Get the email addresses and birthdays of users born between 1980 and 1989
+ */
 SELECT email, birthday
 FROM users
 WHERE birthday >= '1980-01-01'
   AND birthday <= '1989-12-31';
 
--- 3
--- Find the emails and creation date of users 
--- whose created_at date matches this condition.
+/*
+ * Query 3: Get the emails and creation date of users created before May 1, 2017
+ */
 SELECT email, created_at
 FROM users
 WHERE created_at < '2017-05-01';
 
---4
--- There was an A/B test performed on users that used cute animal clipart to encourage users to sign up. We’d like to see how the group that was shown ‘bears’ is performing.
--- Find the emails of the users who received the ‘bears’ test.
+/*
+ * Query 4: Get the emails of users who were part of the 'bears' A/B test
+ * This test used cute animal clipart to encourage users to sign up.
+ */
 SELECT email
 FROM users
 WHERE test = 'bears';
 
-
---5
--- A total of 4 advertising campaigns were run over this period.
--- There were two sets of ad copy (set 1 and set 2) and both were run on two search engine sites (AAA and BBB).
--- The resulting campaign values are:
--- AAA-1
--- AAA-2
--- BBB-1
--- BBB-2
--- Find all the emails of all users who received a campaign on website BBB.
+/*
+ * Query 5: Get the emails of users who were part of a campaign on website BBB
+ * There were 4 advertising campaigns run, with two sets of ad copy (set 1 and set 2), both run 
+ * on two search engine sites (AAA and BBB).
+ */
 SELECT email
 FROM users
 WHERE campaign LIKE 'BBB%';
 
--- 6
--- Find all the emails of all users who received ad copy 2 in 
--- their campaign.
+/*
+ * Query 6: Get the emails of users who received ad copy 2 in their campaign
+ */
 SELECT email
 FROM users
 WHERE campaign LIKE '%-2';
 
--- 7
--- Find the emails for all users who received both a campaign and a test. 
--- These users will have non-empty entries in the 
--- campaign and test columns.
+/*
+ * Query 7: Get the emails of users who received both a campaign and a test
+ */
 SELECT email
 FROM users
 WHERE campaign IS NOT NULL
